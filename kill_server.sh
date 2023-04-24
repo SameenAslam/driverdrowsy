d9156c1ccbe823534bdfe98919a07906e07986f8
SameenAslam <64708074+SameenAslam@users.noreply.github.com>
2023-04-24 23:18:07 +0500

    Add files via upload

---
+++ kill_server.sh
@@ -0,0 +1,6 @@
+portFind="$1"
+echo "Finding process on port: $portFind"
+pid=$(netstat -tulnp | grep :"$1" | awk '{print $7}' | cut -f1 -d"/")
+echo "Process found: $pid"
+kill -9 $pid
+echo "Process $pid killed"
